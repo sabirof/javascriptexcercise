52fdbe39a25e5e89db77c3a137b6d1a36a5d8261
sabirof <maharram.sabir@gmail.com>
2023-03-27 10:35:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+//console.log("Starting Javascript gurl");
+//Excercise 1
+// var myName = "Sabir";
+// console.log("My name is " + "" + myName);
+
+
+//Exercise 2
+//Create a variable called age with a number that is your age. Do not use string quotes for numbers.
+//Add a line to print that variable in the console. Save the file and reload the page. You should see your name and your age.
+// var myAge = 32;
+// //console.log(myName + " is " + myAge);
+
+
+// //Excercise 3
+// //Create a variable called juliaAge with a value 32. Create another variable called ageDiff and set it to an expression that calculates your age minus Julias's age. 
+// //Print the value of ageDiff.Save the file and reload the page.You should see your age and the age difference.If you are younger than Julia, you should see a negative number.
+// var juliaAge = 32
+//var ageDiff = juliaAge - myAge
+//console.log(ageDiff);
+
+//excercise 4
+//Write a conditional that compares the variable with your age with the number 21. It should print either "You are older than 21" or "You are not older than 21", appropriately, depending on your age. Save your JavaScript file and reload the page. Make sure you see the correct message. Try changing your age in the JavaScript file to make sure the other message prints when it should.
+
+    //if (myAge > 21) {
+        //message = "You are older than 21";
+    //} else {
+        //message = "You are a kiddo"
+    //}
+
+//console.log(message);
+
+//Exercise 5
+//Write a conditional that compares your age with Julia's age. This conditional will need to test if you are older, younger, or the same age, and print, appropriately, either "Julia is older than you", "Julia is younger than you", or "You have the same age as Julia".
+// if (myAge > juliaAge) {
+//     message = "Julia is younger than you!"
+// } else if (myAge < juliaAge) {
+//     message = "Julia is older than you!"
+// } else {
+//     message = "You are twins!!!"
+// }
+// console.log(message);
+
+//excercise 6 Arrays
+ const humans = ["Alireza", "Martha", "Melissa", "Mila", "Phuoc", "Raul", "Sabir", "Sonja"];
+//console.log(humans[0]);
+//console.log(humans[7]);
+for (let i = 0; i < 1; i++) {
+   console.log(humans); 
+}
+
+
+//Excercise 7 Looping over an Array
+// const ages = [21,22,23,24,25,26,27,28];
+// let i = 0;
+// let agesLength = ages.length;
+// while (i < agesLength) {
+//     if (ages[i] % 2 === 0) {
+//         console.log(ages[i]);
+//     } i++;
+// } 
